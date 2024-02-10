834f73758ca8b193ef63746f275b1a054451556c
Anton Nikitin <antonnikitin@live.co.uk>
2024-02-10 14:35:06 +0000

    change name to core clocks

---
+++ hal/inc/core/hal_core_clocks.h
@@ -0,0 +1,27 @@
+#ifndef HAL_CORE
+#define HAL_CORE
+
+/** \file
+ * 
+ * File containing functions 
+ */
+
+#include "../hal_result.h"
+
+/** Initalise non-peripheral related MCU clocks.
+ *
+ *  \retval #HAL_SUCCESS Initialisation was successful.
+ *  \retval #HAL_ERROR_REJECTED Initialisation failed due a previous `hal_clock_init()` call being made.
+ *  \retval #HAL_ERROR_DEVICE_ERROR Initialisation failed due to a device error.
+ *  \retval #HAL_ERROR_PERIPHERAL_ERROR Initialisation failed due to a peripheral error.
+ */
+hal_result_t hal_clock_init(void);
+
+/** Teardown resources set up by `hal_clock_init()`.
+ * 
+ *  \retval #HAL_SUCCESS Teardown was successful.
+ *  \retval #HAL_ERROR_REJECTED Teardown failed due to a previous `hal_clock_init()` call not being made.
+ */
+hal_result_t hal_clock_teardown(void);
+
+#endif
