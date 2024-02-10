773031ee9659047dce4a6174291b8b780cba759e
Anton Nikitin <antonnikitin@live.co.uk>
2024-02-10 12:55:48 +0000

    documentation

--- hal/inc/hal.h
+++ hal/inc/hal.h
@@ -13,7 +13,7 @@
  *  \warning This function must be called before using any other HAL function.
  * 
  *  \retval #HAL_SUCCESS Initialisation was successful.
- *  \retval #HAL_ERROR_REJECTED Initialisation failed due a previous hal_init call being made.
+ *  \retval #HAL_ERROR_REJECTED Initialisation failed due a previous `hal_init()` call being made.
  *  \retval #HAL_ERROR_DEVICE_ERROR Initialisation failed due to a device error.
  *  \retval #HAL_ERROR_PERIPHERAL_ERROR Initialisation failed due to a peripheral error.
  */
@@ -22,7 +22,7 @@ hal_result_t hal_init(void);
 /** Teardown the HAL. Often the last function to be called.
  * 
  *  \retval #HAL_SUCCESS Teardown was successful.
- *  \retval #HAL_ERROR_REJECTED Teardown failed due to a previous hal_init call not being made.
+ *  \retval #HAL_ERROR_REJECTED Teardown failed due to a previous `hal_init()` call not being made.
  */
 hal_result_t hal_teardown(void);
 
