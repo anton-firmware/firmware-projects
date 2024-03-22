55e2698a5747a5ce2c4b9f6fb8dfe46799ba28a9
Anton Nikitin <antonnikitin@live.co.uk>
2024-03-22 13:26:53 +0000

    inital upload of pack and hal_gpio

---
+++ mcu/atsamd11d14/hal_gpio.c
@@ -0,0 +1,31 @@
+#include "peripherals/hal_gpio.h"
+
+hal_result_t hal_gpio_init(hal_gpio_init_t init_struct)
+{
+
+}
+
+hal_result_t hal_gpio_clock_init(void)
+{
+
+}
+
+hal_result_t hal_gpio_clock_teardown(void)
+{
+
+}
+
+hal_result_t hal_gpio_teardown(void)
+{
+
+}
+
+hal_result_t hal_gpio_set_level(hal_gpio_pin_t pin, hal_gpio_level_t level)
+{
+
+}
+
+hal_gpio_level_t hal_gpio_read_level(hal_gpio_pin_t pin)
+{
+
+}
