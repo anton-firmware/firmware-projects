e3a4c61ae19e821078b9a38dc2d70a9df7496efc
Anton Nikitin <antonnikitin@live.co.uk>
2024-04-20 15:28:26 +0100

    serial header update

--- hal/inc/peripherals/hal_serial.h
+++ hal/inc/peripherals/hal_serial.h
@@ -3,9 +3,32 @@
 
 /** \file
  *
- * File containing definitions for serial functions.
- *
- * \warning Currently only supports host mode.
+ * File containing definitions for serial (UART) functions.
  */
+
+#include "../hal_result.h"
+
+typedef enum hal_serial_mode
+{
+    HAL_SERIAL_MODE_RX,    /*!< Serial RX Only mode. */
+    HAL_SERIAL_MODE_TX,    /*!< Serial TX Only modee. */
+    HAL_SERIAL_MODE_RX_TX, /*!< Serial RX/TX mode. */
+
+} hal_serial_mode_t;
+
+typedef enum hal_serial_stop_bits
+{
+    HAL_SERIAL_STOP_BITS_ONE, /*!< Serial 1 stop bit. */
+    HAL_SERIAL_STOP_BITS_TWO, /*!< Serial 2 stop bits. */
+
+} hal_serial_stop_bits_t;
+
+typedef enum hal_serial_parity_bits
+{
+    HAL_SERIAL_PARITY_BITS_NONE, /*!< Serial no parity bits. */
+    HAL_SERIAL_PARITY_BITS_ONE,  /*!< Serial one parity bit. */
+
+} hal_serial_parity_bits_t;
+
 
 #endif /* HAL_SERIAL_H */
