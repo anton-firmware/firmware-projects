e9109f5954868a8de51909ae3fcdfe4b8ac6e7a8
Anton Nikitin <antonnikitin@live.co.uk>
2024-04-20 12:32:00 +0100

    add hal_serial header

---
+++ hal/inc/peripherals/hal_serial.h
@@ -0,0 +1,11 @@
+#ifndef HAL_SERIAL_H
+#define HAL_SERIAL_H
+
+/** \file
+ *
+ * File containing definitions for serial functions.
+ *
+ * \warning Currently only supports host mode.
+ */
+
+#endif /* HAL_SERIAL_H */
