8095bac0ccf1dbb2542f8fa0eb36bad322cd8426
Anton Nikitin <antonnikitin@live.co.uk>
2024-05-22 20:11:33 +0100

    initial hal serial and makefile changes

--- mcu/atsamd11d14/peripherals/hal_serial.c
+++ mcu/atsamd11d14/peripherals/hal_serial.c
@@ -7,3 +7,17 @@ hal_result_t hal_serial_init(hal_serial_init_t init_struct)
 
     return result;
 }
+
+hal_result_t hal_serial_clock_init()
+{
+    hal_result_t result = HAL_ERROR_PERIPHERAL_ERROR;
+
+    return result;
+}
+
+hal_result_t hal_serial_teardown()
+{
+    hal_result_t result = HAL_ERROR_PERIPHERAL_ERROR;
+
+    return result;    
+}
