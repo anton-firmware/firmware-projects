18f80b1235eac54ed8203afec1bf272385df2b75
Anton Nikitin <antonnikitin@live.co.uk>
2024-11-16 13:46:15 +0000

    systick timer progress

--- mcu/atsamd11d14/peripherals/hal_timer.c
+++ mcu/atsamd11d14/peripherals/hal_timer.c
@@ -21,19 +21,35 @@
 
 // } hal_timer_init_t;
 
+static hal_timer_event_cb_t callback;
+
 hal_result_t hal_timer_init(hal_timer_init_t *init_struct)
 {
     /* The SYSTICK calibration value is incorrect. Errata reference: 14157. 
      * The correct SYSTICK calibration value is 0x40000000. 
      * See 38.2.4 Device ATSAMD11 reference manual.*/
 
-    /* SYSTICK using processor clock. */
-    SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
+    const uint32_t reload_value = (DEFAULT_SYSTEM_CLOCK_HZ / init_struct->period) - 1u;
+
+    /* SYSTICK using processor clock, enable tick interrupt. */
+    SysTick->CTRL |= (SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk);
+
+    /* SYSTICK load value according to passed period. */
+    SysTick->LOAD = reload_value;
+
+    /* Assign callback if present. */
+    if (init_struct->call_back)
+    {
+        callback = init_struct->call_back;
+    }
+    
+    return HAL_SUCCESS;
 }
 
 hal_result_t hal_timer_clock_init(void)
 {
-
+    /* Uses default system clock. */
+    return HAL_SUCCESS;
 }
 
 hal_result_t hal_timer_teardown(void)
@@ -43,7 +59,8 @@ hal_result_t hal_timer_teardown(void)
 
 hal_result_t hal_timer_clock_teardown(void)
 {
-
+    /* Uses default system clock. */
+    return HAL_SUCCESS;
 }
 
 hal_result_t hal_timer_delay(uint16_t ms)
@@ -53,10 +70,10 @@ hal_result_t hal_timer_delay(uint16_t ms)
 
 hal_result_t hal_timer_start(void)
 {
-
+    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
 
 hal_result_t hal_timer_stop(void)
 {
-
+    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
 }
