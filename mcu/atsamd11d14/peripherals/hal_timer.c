3655d312c2b0defa43f03645235c857047705387
Anton Nikitin <antonnikitin@live.co.uk>
2024-11-16 10:47:18 +0000

    add initial hal timer source

---
+++ mcu/atsamd11d14/peripherals/hal_timer.c
@@ -0,0 +1,47 @@
+/** \file
+ *
+ * Implementation for HAL timer peripheral.
+ * 
+ * \warning Currently only supports a simple timer on SYSTICK.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "hal_timer.h"
+#include "sam.h"
+
+hal_result_t hal_timer_init(hal_timer_init_t *init_struct)
+{
+    
+}
+
+hal_result_t hal_timer_clock_init(void)
+{
+    
+}
+
+hal_result_t hal_timer_teardown(void)
+{
+    
+}
+
+hal_result_t hal_timer_clock_teardown(void)
+{
+    
+}
+
+hal_result_t hal_timer_delay(uint16_t ms)
+{
+    
+}
+
+hal_result_t hal_timer_start(void)
+{
+    
+}
+
+hal_result_t hal_timer_stop(void)
+{
+    
+}
