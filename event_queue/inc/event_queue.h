d187cfcd93c8bc0c706a5078f1bddadd55613990
Anton Nikitin <antonnikitin@live.co.uk>
2025-01-22 21:43:08 +0000

    initial header for event queue

--- event_queue/inc/event_queue.h
+++ event_queue/inc/event_queue.h
@@ -1,4 +1,22 @@
 #ifndef EVENT_QUEUE_H
 #define EVENT_QUEUE_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct 
+{
+    uint8_t id;
+} event_t;
+
+typedef struct 
+{
+    size_t buffer_length;
+    uint8_t *buffer;
+    uint16_t head;
+    uint16_t tail;
+} event_queue_t;
+
+bool event_queue_initialise(uint8_t *buffer)
+
 #endif
