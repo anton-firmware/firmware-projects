40f18dfd6b7042230420e7bcd5298a54626cdd20
Anton Nikitin <antonnikitin@live.co.uk>
2025-01-25 00:18:01 +0000

    event queue progress

--- event_queue/inc/event_queue.h
+++ event_queue/inc/event_queue.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct 
 {
@@ -12,11 +13,21 @@ typedef struct
 typedef struct 
 {
     size_t buffer_length;
-    uint8_t *buffer;
+    event_t *buffer;
     uint16_t head;
     uint16_t tail;
 } event_queue_t;
 
-bool event_queue_initialise(uint8_t *buffer)
+bool event_queue_initialise(uint8_t *buffer, size_t length);
+
+bool event_queue_teardown(void);
+
+bool event_queue_enqueue(event_t event);
+
+bool event_queue_enqueue_critical(event_t event);
+
+bool event_queue_dequeue(event_t *event);
+
+bool event_queue_dequeue_critical(event_t *event);
 
 #endif
--- event_queue/src/event_queue.c
+++ event_queue/src/event_queue.c
@@ -1,1 +1,83 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
 #include "event_queue.h"
+#include "hal_core_interrupts.h"
+
+static volatile event_queue_t queue;
+
+static inline bool is_queue_full(void)
+{
+    return ((queue.tail + 1u) % queue.buffer_length) == queue.head;
+}
+
+static inline bool is_queue_empty(void)
+{
+    return queue.tail == queue.head;
+}
+
+bool event_queue_initialise(event_t *buffer, size_t length)
+{
+    queue.buffer = buffer;
+    queue.buffer_length = length;
+    queue.head = 0u;
+    queue.tail = 0u;
+}
+
+bool event_queue_teardown(void)
+{
+    queue.buffer = NULL;
+    queue.buffer_length = 0;
+    queue.head = 0u;
+    queue.tail = 0u;
+}
+
+bool event_queue_enqueue(event_t event)
+{
+    bool result = false;
+
+    if (!is_queue_full())
+    {
+        queue.buffer[queue.head] = event;
+        queue.head = (queue.head + 1u) % queue.buffer_length;
+        result = true;
+    }
+    
+    return result;
+}
+
+bool event_queue_enqueue_critical(event_t event)
+{
+    bool result = false;
+
+    hal_interrupts_disable();
+    result = event_queue_enqueue(event);
+    hal_interrupts_enable();
+    
+    return result;
+}
+
+bool event_queue_dequeue(event_t *event)
+{
+    bool result = false;
+
+    if (event != NULL && !is_queue_empty())
+    {
+        *event = queue.buffer[queue.tail];
+        queue.tail = (queue.tail + 1u) % queue.buffer_length;
+    }
+
+    return result;
+}
+
+bool event_queue_dequeue_critical(event_t *event)
+{
+    bool result = false;
+
+    hal_interrupts_disable();
+    result = event_queue_dequeue(event);
+    hal_interrupts_enable();
+    
+    return result;
+}
