7d2aa6030851569de0f582f5555e3ac93e097a83
Anton Nikitin <antonnikitin@live.co.uk>
2025-01-26 00:58:57 +0000

    populate buran pin define with pcb pins

--- project_buran_fw/buran_defs/buran.h
+++ project_buran_fw/buran_defs/buran.h
@@ -1,14 +1,28 @@
 /*
  * buran.h
  *
+ * Definitions of pins for the Buran PCB.
  * 
  * Created: 12/01/2025 00:20:17
  *  Author: Anton
  */ 
 
-
+ 
 #ifndef BURAN_H
 #define BURAN_H
 
+#define BURAN_PORT_A_INDEX    0u
+
+#define BURAN_DEBUG_LED_1_PIN 5u
+#define BURAN_DEBUG_LED_2_PIN 6u
+#define BURAN_DEBUG_1_PIN     16u
+#define BURAN_DEBUG_2_PIN     9u
+#define BURAN_DEBUG_3_PIN     8u
+#define BURAN_DEBUG_4_PIN     7u
+
+#define BURAN_SWITCH_1_PIN    25u
+#define BURAN_SWITCH_2_PIN    24u
+#define BURAN_SWITCH_3_PIN    2u
+#define BURAN_SWITCH_4_PIN    3u
 
 #endif /* BURAN_H_ */
