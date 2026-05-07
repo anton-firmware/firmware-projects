00572d929bf5fca23f81c0681e7970d0746c0c15
Anton Nikitin <antonnikitin@live.co.uk>
2026-05-07 11:21:05 +0100

    add project venera

---
+++ project_venera_fw/dev_board_xplained_pro/VeneraUSBTest/main.c
@@ -0,0 +1,20 @@
+/*
+ * VeneraUSBTest.c
+ *
+ * Created: 07/05/2026 10:45:30
+ * Author : Anton
+ */ 
+
+
+#include "sam.h"
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
