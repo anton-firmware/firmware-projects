aa41a5f319d695fdaa48dba32e2230aef456b88b
Anton Nikitin <antonnikitin@live.co.uk>
2026-05-09 15:05:38 +0100

    add not implemented hal result code

--- hal/inc/hal_result.h
+++ hal/inc/hal_result.h
@@ -63,6 +63,9 @@ typedef uint8_t hal_result_t;
 /** Value representing an unsupported operation. */
 #define HAL_ERROR_UNSUPPORTED ((hal_result_t)136)
 
+/** Value representing an unsupported operation. */
+#define HAL_ERROR_NOT_IMPLEMENTED ((hal_result_t)137)
+
 /** \} End of Failure codes. */
 
 #endif /* HAL_RESULT_H */
